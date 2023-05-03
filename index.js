88b12e2ccc308d8c290f5c1374cbd23ec3ff176c
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-03 15:25:14 +0300

    Add basic data types

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+  коментарий
+*/
+// коментарий
+alert(150); // число
+alert(513n); // BigInt
+// строки
+alert('string 1');
+alert("string 2");
+alert(`string 3`);
+// boolean
+alert(true); 
+alert(false); 
+
+alert(undefined);
+alert(null);
