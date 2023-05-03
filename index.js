d8de00f98ac63eba2cc0f966a8fe892ac736217b
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-03 16:03:37 +0300

    Add variable creation examples

--- index.js
+++ index.js
@@ -6,11 +6,35 @@ console.log(150); // число
 console.log(513n); // BigInt
 // // строки
 console.log('string 1');
-console.log("string 2");
+console.log('string 2');
 console.log(`string 3`);
 // // boolean
-console.log(true); 
-console.log(false); 
+console.log(true);
+console.log(false);
 
 console.log(undefined);
 console.log(null);
+
+// обьявление переменной с названием box
+let box;
+
+// присвоение переменной box конкретного значения
+box = 10;
+
+console.log(box);
+
+// инициализация переменной
+let box2 = 'test 12345';
+
+console.log(box2);
+
+let longVariableName = 12345;
+
+let _text; 
+let _; // Lodash
+let $; // JQuery
+
+// let 1234test;
+
+// let TEST;
+// let test;
