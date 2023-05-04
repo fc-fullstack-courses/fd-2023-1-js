31e844efeb170fec6a46ddb78bf7cde7c078d223
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-04 14:48:42 +0300

    Add typeof examples

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ let res1 = 2 * 2 + 2; // 6
 let res2 = 2 * (2 + 2); // 8
 
 console.log(res2); // вызов функции
+
+// typeof
+const type1 = typeof 'sdadsadad'; // 'string'
+console.log(type1);
+console.log(typeof undefined); // 'undefined'
+console.log(typeof 4648469); // 'number'
+console.log(typeof 123434n); // 'bigint'
+console.log(typeof true) // 'boolean'
+console.log(typeof console.log); // 'function'
+console.log(typeof {}); // 'object'
+console.log(typeof null) // 'object' 
+
