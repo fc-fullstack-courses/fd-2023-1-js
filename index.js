a7dde0e602d60e8bf89d6945e1567b9409b16ee5
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-05 15:20:39 +0300

    Add if ... else NaN check

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-console.log('dfdsfdsg');
+// console.log('dfdsfdsg');
 
 // alert('asdaff');
 
-let test = 12343;
+// let test = 12343;
 
-console.log(test);
+// console.log(test);
 
 // 46456;
 
@@ -15,6 +15,24 @@ console.log(test);
   выведите в консоли / алерте следующее сообщение:
   Привет, Пользователь
 */
-let userInput = prompt('Введите имя');
-let greetingText = 'Привет, ' + userInput;
-console.log(greetingText);
+// let userInput = prompt('Введите имя');
+// let greetingText = 'Привет, ' + userInput;
+// console.log(greetingText);
+
+
+// let value = NaN;
+// alert(value !== value); 1
+// const isNaNAnswer = isNaN(value); 2
+
+let number1 = +prompt('Enter number1');
+let number2 = +prompt('Enter number2');
+
+let result = number1 + number2;
+
+const isNaNResult = isNaN(result);
+
+if (isNaNResult) {
+  alert('Incorrect data. Please try again');
+} else {
+  alert(result);
+}
