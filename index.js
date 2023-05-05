1d09ba09e6167803e72788f7776e1046b6cf7419
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-05 14:37:25 +0300

    Add prompt example

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('dfdsfdsg');
+
+// alert('asdaff');
+
+let test = 12343;
+
+console.log(test);
+
+// 46456;
+
+// функция, которая позволяет пользователю ввести что-то
+let userInput = prompt('Введите имя');
+
