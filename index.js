b1b270b760081669d42d6778eb6d7f1be433b4dd
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-05 14:49:11 +0300

    Add minitask solution

--- index.js
+++ index.js
@@ -9,5 +9,12 @@ console.log(test);
 // 46456;
 
 // функция, которая позволяет пользователю ввести что-то
-let userInput = prompt('Введите имя');
 
+/*
+  получите имя пользователя
+  выведите в консоли / алерте следующее сообщение:
+  Привет, Пользователь
+*/
+let userInput = prompt('Введите имя');
+let greetingText = 'Привет, ' + userInput;
+console.log(greetingText);
