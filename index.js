abaa7e61cb5aae138dcb09d6c5a0cf35fa2d18fb
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-08 16:44:28 +0300

    Add basic switch ... case construction

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-debugger;
+// debugger;
 let condition = NaN;
 let condition2 = false;
 
@@ -25,4 +25,33 @@ if (condition) {
   // выполняется во всех других случаях. Всегда пишется в конце
   // конструкции
   console.log('this is else');
+}
+
+// switch ... case
+const userOrder = prompt(
+  'Choose your product \n1 - tea \n2 - coffee \n3 - juice'
+);
+// debugger;
+switch (userOrder) {
+  // описываем случаи / значения которые принимает userOrder
+  // и сравнивает их по строгому стравнению (===)
+  case '1': {
+    // код для случая userOrder === '1'
+    alert('You have ordered tea');
+    // отключает падение дальше по кейсам
+    break;
+  }
+  case '2': {
+    alert('You have ordered coffee');
+    break;
+  }
+  case '3': {
+    alert('You have ordered juice');
+    break;
+  }
+  // для всех остальных случаев
+  // в него тоже мон упасть если не делать break
+  default: {
+    alert('incorrect product number');
+  }
 }
