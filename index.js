bdb8e2a7ed2559dbb8842207e99a53ba007f089d
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-09 14:59:54 +0300

    Add basic while loop

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 'use strict';
+// console.log('iteration');
+// console.log('iteration');
+// console.log('iteration');
+// console.log('iteration');
+// console.log('iteration');
+
+// let condition = false;
+
+let counter = 0;
+while (counter < 5) {
+  // тело цикла
+  counter++;
+  console.log('iteration');
+}
