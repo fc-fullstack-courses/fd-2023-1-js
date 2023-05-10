8f71613a44e035c830c07397faa61b7d92c4b2e1
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-10 14:55:17 +0300

    Add object complexity examples

--- index.js
+++ index.js
@@ -11,9 +11,15 @@ const car = {
   model: 'Model XYT++',
   power: 430,
   isUsed: false,
-
+  dimensions : {
+    width: 3,
+    height: 2,
+    lenght: 7
+  },
+  otherObject: object,
   drive: function () {
     // метод обьекта
     return 'car is driving';
   },
 };
+
