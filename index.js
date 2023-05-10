619a399734b38b6582bd5ba5bb35b987ed7883d5
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-10 14:52:03 +0300

    Add basic object examples

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const object = {
+  key: 'value',
+  display: 'flex',
+  justiftContent: 'center',
+};
+
+const car = {
+  color: 'red',
+  model: 'Model XYT++',
+  power: 430,
+  isUsed: false,
+
+  drive: function () {
+    // метод обьекта
+    return 'car is driving';
+  },
+};
