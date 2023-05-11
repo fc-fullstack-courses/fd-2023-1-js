e7dd42cef15ce9d2f1344724cbdd8735126e364a
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-11 15:22:24 +0300

    Add tasks solution

--- index.js
+++ index.js
@@ -6,9 +6,38 @@ let b = 20;
 // 10 + 20 = 30
 
 // alert(a + ' + ' + b + ' = ' + (a + b));
-alert(`${a} + ${b} = ${a + b}`);
+// alert(`${a} + ${b} = ${a + b}`);
 
 // alert('Пролог\nЖили-были');
 // alert(`Пролог
 // Жили-                           
 //   были`);
+
+/*
+  вывести в консоль / алерт строчки:
+  a - b = result
+  a * b = result
+  a / b = result
+*/
+
+const user = {
+  firstName: 'Test',
+  lastName: 'User',
+  age: 12,
+  isMale: true
+}
+const user2 = {
+  firstName: 'User',
+  lastName: 'Test',
+  age: 12,
+  isMale: true
+}
+
+/*
+  написать функцию, которая будет создавать строчку
+  в которой будут здороватся с пользователем
+*/
+
+const greetUser = function(user) {
+  return `Hello, ${user.firstName} ${user.lastName}!`;
+}
