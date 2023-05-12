26bdbcf4eabc5f49952a246cd17ebdae6c86380e
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-12 15:47:47 +0300

    Add array examples

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 'use strict';
+
+// const user1Email = 'mail1@test.test';
+// const user2Email = 'mail2@test.test';
+// const user3Email = 'mail3@test.test';
+
+// const userEmails = {
+//   0: 'mail1@test.test',
+//   1: 'mail2@test.test',
+//   2: 'mail3@test.test',
+//   // 1: 'mail4@test.test'
+// };
+
+// const userEmailsArr = new Array(
+//   'mail1@test.test',
+//   'mail2@test.test',
+//   'mail3@test.test'
+// );
+
+// userEmailsArr[40] = 'mail4@test.test';
+
+const userEmails = [
+  'mail1@test.test',
+  'mail2@test.test',
+  'mail3@test.test',
+  'mail4@test.test',
+];
