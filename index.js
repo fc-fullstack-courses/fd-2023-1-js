9aac3eb4d1cfb864ae5114990c80eb7bf356d032
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-17 15:01:29 +0300

    Add error throwing example

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function sum(num1, num2) {
+  if (
+    typeof num1 !== 'number' ||
+    typeof num2 !== 'number' ||
+    isNaN(num1 - num2)
+  ) {
+    // const error = new TypeError('You must provide numbers');
+
+    // throw error;
+
+    throw new TypeError('You must provide numbers');
+  }
+
+  return num1 + num2;
+}
+
+console.log(1);
+
+const result = sum(true, true);
+// throw 10;
+console.log(2);
