d5f4d011b13d092615cd3fb4af6dae73e9ad2b1b
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-17 17:31:47 +0300

    Add string methods

--- index.js
+++ index.js
@@ -21,3 +21,34 @@
 
 // +prompt()
 // Number( prompt() )
+
+// const str1 = String(false);
+// '' + false;
+
+const str = 'test test';
+
+// str.charAt(2) === str[2]
+
+// значене символа в Юникоде
+const unicodeValue = str.charCodeAt(2);
+
+const newStr = str.concat(' new text');
+
+const includes1 = str.includes('test');
+const includes2 = str.includes('text');
+
+const index1 = str.indexOf(' ');
+const index2 = str.indexOf('text');
+
+const str2 = str.replace('test', 'replaced');
+
+const sliced = str2.slice(0, -5);
+
+const capsedStr = str.toUpperCase();
+const lowerStr = capsedStr.toLowerCase();
+
+const badStr = '                                    Anton              User                                                                                                 ';
+
+const trimmed = badStr.trim();
+
+// const splitted = str.split();
