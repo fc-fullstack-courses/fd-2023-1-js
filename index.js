df1b0d281e82ff6c970f3cbfae6904cf7c859cad
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-18 17:05:03 +0300

    Add spread operator

--- index.js
+++ index.js
@@ -39,3 +39,23 @@ function multiply(...numbers) {
   return result;
 }
 
+const numbersArr = [50, 454, 8, 9879, -500];
+const arr2 = ['test', 'test2', 'test3'];
+
+const user = {
+  test: 1,
+  test2: 2,
+};
+
+const arr3 = [...numbersArr, null, ...arr2, ...'tesagd', ...user];
+
+// const maxNumber = Math.max(
+//   numbersArr[0],
+//   numbersArr[1],
+//   numbersArr[2],
+//   numbersArr[3],
+//   numbersArr[4]
+// );
+
+const maxNumber = Math.max(...numbersArr);
+const res3 = multiply(...numbersArr);
