b0df313c5f533b4c6abe45a77ec0cd7a6b8f4675
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-29 16:12:09 +0300

    Add Worker task solution

--- index.js
+++ index.js
@@ -21,3 +21,29 @@ class User {
 }
 
 const user1 = new User('login1', 'pass1', 19);
+
+/*
+  создать класс Worker
+  у сотрудников есть
+    имя
+    фамилия
+    ежедневная ставка
+    колисчество отработанных дней
+
+  у сотрудников есть метод, который будет возвращать размер зарплаты, которую необходимо оплатить за пройденное время
+*/
+
+class Worker {
+  constructor(firstName, lastName, daysWorked, paymentRate) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.daysWorked = daysWorked;
+    this.paymentRate = paymentRate;
+  }
+
+  getPayment () {
+    return this.daysWorked * this.paymentRate;
+  }
+}
+
+const worker1 = new Worker('Test', 'Testenko', 5, 100);
