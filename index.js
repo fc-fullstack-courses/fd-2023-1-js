9fb30077146dcfe8fcde8d5299acbaac6354392e
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-02 16:34:59 +0300

    Add pop method

--- index.js
+++ index.js
@@ -23,11 +23,47 @@ class MyArray {
 
     return this.length;
   }
+
+  pop() {
+    /*
+      1. если массив пустой (длина = 0) 
+        1.1 вернуть undefined
+      2. если массив не пустой:
+        2.1 сохранить удаляемый элемент
+        2.2 удаляем последнее свойство 
+        2.3 уменшить длину на 1
+        2.4 вернуть удаляемый элемент
+    */
+
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    const deletedElem = this[this.length -1];
+
+    delete this[this.length -1];
+
+    this.length--;
+
+    return deletedElem;
+  }
 }
 
-const arr = [];
+const arr = [40, 53, 46];
 const myArr = new MyArray();
 
-myArr.push(16546);
-myArr.push('text');
-myArr.push(true, null, false);
+myArr.push(40, 53, 46);
+
+const arrDeleted1 = arr.pop();
+const arrDeleted2 = arr.pop();
+
+console.log(arrDeleted1);
+console.log(arrDeleted2);
+console.log(arr);
+
+const myArrDeleted1 = myArr.pop();
+const myArrDeleted2 = myArr.pop();
+
+console.log(myArrDeleted1);
+console.log(myArrDeleted2);
+console.log(myArr);
