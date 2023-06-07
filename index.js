14173d784123186a7c83f8442bee6bce0d84073b
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-07 14:25:22 +0300

    Add basic recursion function examples

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function recursive() {
+  debugger;
+  // ...
+  recursive();
+}
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+function nonRecursive() {
+  debugger;
+
+  sum(2,5);
+}
