19bc053ffdbd76e59f998577d9d1dfdeb3bd8d77
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-07 14:59:03 +0300

    Add recursive functions examples

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
 
+// let x = 0;
+
 function recursive() {
-  debugger;
+  // debugger;
   // ...
+  // x++;
   recursive();
 }
 
@@ -13,5 +16,51 @@ function sum(num1, num2) {
 function nonRecursive() {
   debugger;
 
-  sum(2,5);
+  sum(2, 5);
 }
+
+// for(let i = 10; i > 0; i--) {
+//   console.log(i);
+// }
+
+function countdown(number) {
+  debugger;
+  console.log(number);
+
+  if (number > 0) {
+    countdown(number - 1);
+  }
+
+  return 'done'; // база рекурсии
+}
+
+/*
+  min = 2
+  max = 5
+
+
+  2
+  3
+  4
+  5
+
+*/
+function logRange(min, max) {
+  debugger;
+  if (min > max) {
+    throw new RangeError('min must be smaller than max');
+  }
+
+  console.log(min);
+
+  if (min < max) {
+    logRange(min + 1, max);
+  }
+}
+
+/*
+  Этапы рекурсии:
+    1. функции рекурсивно выполняются в колл стеке
+    2. доходим до базы рекурсии
+    3. все остальные функции завершают работу
+*/
