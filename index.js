538fe4d582a8e4680dced0d3e53f418b4718f9b4
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-08 14:26:13 +0300

    Add scope reminders

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 'use strict';
+
+let value1 = 'test';
+let value3 = 12345;
+
+function log1() {
+  console.log(`log function: ${value3}`);
+}
+
+function log2() {
+  let value2 = 'inner';
+  let value3 = 98765;
+
+  // console.log(`value2: ${value2}`);
+  // console.log(`value3: ${value3}`);
+
+  // log1();
+
+  if (true) {
+    let value3 = 8465468463;
+    console.log(value3);
+  }
+
+  function log3() {
+    console.log(`log3 function: ${value3}`);
+  }
+
+  log3();
+}
+
+// console.log(value2);
