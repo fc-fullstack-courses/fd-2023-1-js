bdd7d70a58b453661a896f70d28b2bfe2421bb9b
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-09 15:58:56 +0300

    Add basic listener example

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// window.document;
+// document
+
+// Что делать
+function btnAlert() {
+  alert('Hello world');
+}
+
+// Кто (поиск элемента)
+const [firstButton] = document.getElementsByTagName('button');
+// const [firstButton] = buttons;
+
+// Когда мы будем что-то делать
+// target.addEventLister(type, listener);
+firstButton.addEventListener('click', btnAlert);
