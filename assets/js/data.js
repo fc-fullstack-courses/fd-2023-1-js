65318bb3f005f888fa20ecffe045cc418b300761
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-15 14:44:00 +0300

    Add actors data

---
+++ assets/js/data.js
@@ -0,0 +1,81 @@
+const responseData = [
+  {
+    id: 1,
+    firstName: 'Jason',
+    lastName: 'Statham',
+    profilePicture:
+      'https://www.themoviedb.org/t/p/w500/whNwkEQYWLFJA8ij0WyOOAD5xhQ.jpg',
+    contacts: [
+      'https://www.facebook.com/JasonStatham/',
+      'https://twitter.com/realjstatham',
+      'https://www.instagram.com/jasonstatham/',
+    ],
+  },
+  {
+    id: 2,
+    firstName: 'Dwayne',
+    lastName: 'Johnson',
+    profilePicture:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Dwayne_Johnson_2%2C_2013.jpg/800px-Dwayne_Johnson_2%2C_2013.jpg',
+    contacts: [
+      'https://www.facebook.com/DwayneJohnson',
+      'https://www.instagram.com/therock/',
+    ],
+  },
+  {
+    id: 3,
+    firstName: 'Emma',
+    lastName: 'Stone',
+    profilePicture:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Emma_Stone_at_he_39th_Mill_Valley_Film_Festival_%28cropped%29.jpg/320px-Emma_Stone_at_the_39th_Mill_Valley_Film_Festival_%28cropped%29.jpg',
+    contacts: [
+      'https://www.facebook.com/DwayneJohnson',
+      'https://www.instagram.com/therock/',
+    ],
+  },
+  {
+    id: 4,
+    firstName: 'Somebody',
+    lastName: 'Tuffak',
+    profilePicture: null,
+    contacts: [
+      'https://twitter.com/realjstatham',
+      'https://www.instagram.com/therock/',
+    ],
+  },
+  {
+    id: 999,
+    firstName: '',
+    lastName: '',
+    profilePicture:
+      'https://m.media-amazon.com/images/M/MV5BMTM3OTUwMDYwNl5BMl5BanBnXkFtZTcwTUyNzc3Nw@@._V1_.jpg',
+    contacts: [
+      'https://twitter.com/realjstatham',
+      'https://www.instagram.com/therock/',
+    ],
+  },
+  {
+    id: 5,
+    firstName: 'Scarlett',
+    lastName: 'Johansson',
+    profilePicture:
+      'https://m.media-amazon.com/images/M/MV5BMTM3OTUwMDYwNl5BMl5BanBnXkFtZTcwNTUyNzc3Nw@@._V1_.jpg',
+    contacts: [
+      'https://www.facebook.com/pages/category/Actor/Scarlett-Johansson-Official-101457158013203/',
+      'https://twitter.com/scarlett_jo',
+      'https://www.instagram.com/scarlett.johansson.fc/',
+    ],
+  },
+  {
+    id: 6,
+    firstName: 'Daniel',
+    lastName: 'Day-Lewis',
+    profilePicture:
+      'https://media.vanityfair.com/photos/59497bf7e9423741a1f17860/master/pass/daniel-day-lewis-quits-acting.jpg',
+    contacts: [
+      'https://www.facebook.com/JasonStatham/',
+      'https://twitter.com/realjstatham',
+      'https://www.instagram.com/jasonstatham/',
+    ],
+  },
+];
