92383eb0f00e864b8c7b2320bd88097f4d20cafc
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-16 15:36:09 +0300

    Add async examples

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const btn = document.getElementById('btn');
+
+// btn.addEventListener('click', () => {
+//   // while (true) {
+
+//   // }
+//   let j = 2;
+
+//   for(let i = 0; i < 20_000_000_000; i++) {
+//     j *= j;
+//   }
+
+//   setTimeout(() => {console.log('ok')}, 2000);
+
+// });
+
+
+console.log('first');
+
+setTimeout(()=> {console.log('second')}, 0);
+
+console.log('third');
